8c18d372a93b2e3406be9b520eeaf6ccc3e4e0ae
Prajwal Rangegowda <147972135+PrajwalRangegowda@users.noreply.github.com>
2025-02-02 11:07:57 +0530

    added meta tag to make it work with ios and android

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Notionbees" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" href="/icons/icon-48x48.png" />
         <link rel="icon" type="image/png" href="/icons/icon-48x48.png" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
